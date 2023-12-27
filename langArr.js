40e2a59511a327e66261d898d63f8ac793ec52c4
JuroChek <107763211+JuroChek@users.noreply.github.com>
2023-12-27 10:00:39 +0100

    Add files via upload

---
+++ langArr.js
@@ -0,0 +1,389 @@
+const langArr = {
+    "head-text": {
+        "de": "Lebenslauf",
+        "en": "Resume",
+        "ru": "Резюме",
+    },
+
+    "fname": {
+        "de": "<b>Farbitnyi</b>",
+        "en": "<b>Farbitnyi</b>",
+        "ru": "<b>Фарбитный</b>",
+
+    },
+
+    "name": {
+        "de": "<b>Juryi</b>",
+        "en": "<b>Yurii</b>",
+        "ru": "<b>Юрий</b>",
+    },
+
+    "kontakts-heater": {
+        "de": "<b>Kontaktdaten</b>",
+        "en": "<b>Contacts</b>",
+        "ru": "<b>Контакты</b>",
+    },
+
+    "phone": {
+        "de": "Telefon",
+        "en": "Phone",
+        "ru": "Телефон",
+    },
+
+    "adress": {
+        "de": "Adresse",
+        "en": "Address",
+        "ru": "Адрес",
+    },
+
+    "job-h": {
+        "de": "Über der Job",
+        "en": "About job",
+        "ru": "О работе",
+    },
+
+    "job-1": {
+        "de": "Ich suche einen Job als Frontend- oder Backend- JavaScript Entwickler",
+        "en": "I'm looking for a Frontend or Backend JavaScript developer job",
+        "ru": "Я ищу работу Frontend или Backend JavaScript developer",
+    },
+
+    "job-2": {
+        "de": "Dieses Projekt ist meine Präsentationsarbeit",
+        "en": "This project is my presentation work",
+        "ru": "Данный проект является моей презентационной работой",
+    },
+
+    "job-3": {
+        "de": "Ich werde dieses Projekt ständig weiterzuentwickeln und zu verbessern, während sich mein Erpfarung verbessert",
+        "en": "I will continue to develop and improve this project as my experience improves",
+        "ru": "Данный проект я планирую постоянно разввивать и совершествовать по мере улучшения моих знаний",
+    },
+
+    "job-4": {
+        "de": "Gerade lerne ich JavaScript",
+        "en": "Right now I'm learning JavaScript",
+        "ru": "Прямо сейчас я изчуаю JavaScript ",
+    },
+
+    "job-5": {
+        "de": "Nach Abschluss des JavaScript-Kurses werde ich HTML und CSS detaillierten lernen.",
+        "en": "Upon completion of the JavaScript course, I will move on to a detailed study of HTML and CSS",
+        "ru": "По завершении курса JavaScript перейду к детальному изучению HTML и CSS",
+    },
+
+    "studium-heater": {
+        "de": "Ausbildung",
+        "en": "Education",
+        "ru": "Образование",
+    },
+
+    "language": {
+        "de": "Sprachen",
+        "en": "Languages",
+        "ru": "Языки",
+    },
+
+    "studium1": {
+        "de": "College auf der Grundlage der Kharkiv State Technical University of Construction and Architecture",
+        "en": "College based on Kharkiv State Technical University of Construction and Architecture",
+        "ru": "Лицей на базе ХГТУСА",
+    },
+
+    "studium2": {
+        "de": "Nationale Universität für Bauwesen und Architektur Charkiw",
+        "en": "Kharkiv National University of Construction and Architecture",
+        "ru": "Харьковский государственный национальный университет строительсва и архитектуры",
+    },
+
+    "studium3": {
+        "de": "Deutsch kurs in VHS",
+        "en": "German course in VHS",
+        "ru": "Курс немецкого языка на VHS",
+    },
+
+    "studium5": {
+        "de": "Russisch, Ukrainisch, Englisch (A1), Deutsch (A2)",
+        "en": "Russian, Ukrainian, English(A1), German(A2)",
+        "ru": "Русский, Украинский, Английский(A1), Немецкий(A2)",
+    },
+    
+    "expiriens": {
+        "de": "Berufserfahrung",
+        "en": "Job experience",
+        "ru": "Опыт работы",
+    },
+   
+    "job1": {
+        "de": "Arbeit im Unternehmen zur Herstellung von Metallkonstruktionen für Aufzüge,",
+        "en": "Work in the company producing metal structures for elevators",
+        "ru": "Работа в компании по производству запастных частей для Лифтов",
+    },
+
+    "job2": {
+        "de": "als Abteilungsleiter.",
+        "en": "as department head.",
+        "ru": "Должность начальник отдела.",
+    },
+
+    "job3": {
+        "de": "Aufgaben:",
+        "en": "Tasks:",
+        "ru": "Задания:",
+    },
+
+    "job4": {
+        "de": "- Unternehmensleitung;",
+        "en": "- company management;",
+        "ru": "- управление отделом;",
+    },
+    "job5": {
+        "de": "- Arbeit mit Finanzen;",
+        "en": "- work with finances;",
+        "ru": "- работа с финансами;",
+    },
+    "job6": {
+        "de": "- Außendienst",
+        "en": "- External sales",
+        "ru": "- внешние продажи;",
+    },
+    "job7": {
+        "de": "- Site leiter;",
+        "en": "- Site manager;",
+        "ru": "- Модератор сайта;",
+    },
+    "job8": {
+        "de": "- Arbeit mit verschiedenen  Metallbearbeitungswerkzeugen, Erfahrungen bei der Metallbearbeitung;",
+        "en": "- work with sophisticated metalworking equipment;",
+        "ru": "- работа с различным металлообрабатывающим оборудованием;",
+    },
+    "job9": {
+        "de": "- Abkanten, Hacken, Schneiden, Stanzen, Bearbeiten;",
+        "en": "- folding, chopping, cutting, punching, machining;",
+        "ru": "- гибка, рубка, резка, пробивка, мехобработка;",
+    },
+    "job10": {
+        "de": "- Pulverbeschichten;",
+        "en": "- powder coating;",
+        "ru": "- системы порошковой окраски изделий;",
+    },
+    "job11": {
+        "de": "- Anordnung der Prozessroute;",
+        "en": "- arrangement of the process route;",
+        "ru": "- организация технологического маршрута изготовления изделий",
+    },
+    "job12": {
+        "de": "- Entwicklung der Designdokumentation;",
+        "en": "- Development of design documentation;",
+        "ru": "- Разработка проектно-конструкторской документации;",
+    },
+    "job13": {
+        "de": "Ingenieurbüro TEP-Sojus.",
+        "en": "Engineering company TEP-Soyuz.",
+        "ru": "Инжиниринговая фирма ТЭП-Союз.",
+    },
+
+    "job14": {
+        "de": "als Ingenieur",
+        "en": "engineer",
+        "ru": "Должность инженер 2 категории",
+    },
+    "job15": {
+        "de": "Aufgaben:",
+        "en": "Tasks:",
+        "ru": "Задания:",
+    },
+    "job16": {
+        "de": "- Entwicklung der Designdokumentation;",
+        "en": "- Development of design documentation;",
+        "ru": "- Разработка проектно-конструкторской документации;",
+    },
+    "job17": {
+        "de": "- Arbeit mit Computernetzwerken",
+        "en": "- Work with computer networks",
+        "ru": "- работа с компьютерными сетями;",
+    },
+    "job18": {
+        "de": "- Arbeiten mit Computer-Aided-Design-Systemen",
+        "en": "- Working with computer-aided design systems",
+        "ru": "- работа с системами автоматизированного проектирования;",
+    },
+    "job19": {
+        "de": "- Geschäftskorrespondenz;",
+        "en": "- business correspondence;",
+        "ru": "- деловая переписка;",
+    },
+    "job20": {
+        "de": "- Dokumentenfluss;",
+        "en": "- document flow;",
+        "ru": "- документооборот;",
+    },
+
+    "job21": {
+        "de": "ISP PSN",
+        "en": "ISP PSN",
+        "ru": "Итернет-провайдер PSN",
+    },
+    "job22": {
+        "de": "Monteur",
+        "en": "Fitter",
+        "ru": "Техник-монтажник",
+    },
+    "job23": {
+        "de": "Aufgaben:",
+        "en": "Tasks:",
+        "ru": "Задания:",
+    },
+    "job24": {
+        "de": "- Verlegung von Internetnetzwerken auf Basis von Twisted Pair;",
+        "en": "- laying Internet networks based on twisted pair;",
+        "ru": "- прокладка интернет сетей на базе витой пары;",
+    },
+    "job25": {
+        "de": "- Einrichten von Routern;",
+        "en": "- setting up routers;",
+        "ru": "- настройка роутеров;",
+    },
+    "job26": {
+        "de": "- Konfigurieren von Betriebssystemen für den Netzwerkbetrieb;",
+        "en": "- Configuring operating systems for network working;",
+        "ru": "- Натройка операционных систем для рабты в сети;",
+    },
+    "expo": {
+        "de": "Erfahrung",
+        "en": "Experience",
+        "ru": "Навыки",
+    },
+    "expo-heat": {
+        "de": "Erfahrung in der IT",
+        "en": "IT Experience",
+        "ru": "Опыт в IT",
+    },
+    "expo1": {
+        "de": "Gute Kenntnisse im Bereich IT:",
+        "en": "Good knowledge of IT:",
+        "ru": "Имею следующие знания в IT:",
+    },
+    "expo2": {
+        "de": "- Frontend-Entwickler, JavaScript, HTML, CSS;",
+        "en": "- Frontend developer, JavaScript, HTML, CSS;",
+        "ru": "- Frontend developer, JavaScript, HTML, CSS;",
+    },
+    "expo3": {
+        "de": "- Aufbau von Computernetzwerken",
+        "en": "- building computer networks.",
+        "ru": "- построение компьютерных сетей;",
+    },
+    "expo4": {
+        "de": "- Erfahrung in der Arbeit mit Servern und Domänen;",
+        "en": "- experience work with servers, Domains;",
+        "ru": "- опыт работы с серверами, доменами;",
+    },
+    "expo5": {
+        "de": "CMS-spezifische Systeme",
+        "en": "CMS specific systems",
+        "ru": "- cистемы управления сайтом CMS;",
+    },
+    "expo6": {
+        "de": "SEO für Suchmaschinen wie Google.",
+        "en": "SEO for search engines like Google.",
+        "ru": "- SEO для поисковой системы Google;",
+    },
+
+    "expo7": {
+        "de": "Computersysteme Windows, Linux Ubuntu.",
+        "en": "Computer systems Windows, Linux Ubuntu.",
+        "ru": "- Компьютерные системы Windows, Linux Ubuntu.",
+    },
+    "expo8": {
+        "de": "Erfahrung in",
+        "en": "Experience in",
+        "ru": "Опыт в",
+    },
+    "expo9": {
+        "de": "Metallverarbeitung ",
+        "en": "steel processing",
+        "ru": "металообработке",
+    },
+    "expo10": {
+        "de": "Kenntnisse der Metallverarbeitung Technologien",
+        "en": "Knowledge of steel processing technologies",
+        "ru": "Имею следующие знания в металообработке:",
+    },
+    "expo11": {
+        "de": "- Metallverarbeitungstechnik (Biegen, Schneiden, Bearbeiten, Schweißen usw.) ",
+        "en": "- steel processing technology (bending, cutting, machining, welding, etc.)",
+        "ru": "- технология обработки металлов (гибка, резка, мехобработка, сварка и т.д.);",
+    },
+    "expo12": {
+        "de": "-Arbeiten mit rostfreien und gewöhnlichen Stählen sowie mit Nichteisenmetallen",
+        "en": "-Working with stainless and common steels as well as non-ferrous metals",
+        "ru": "- работа с нержавеющими и обычными сталями, а также цветными металлами;",
+    },
+    "expo13": {
+        "de": "-Nasslackierung, Pulverlackierung.",
+        "en": "-Wet painting, powder painting.",
+        "ru": "- мокрая покраска и порошковая покраска;",
+    },
+    "hobby-heater": {
+        "de": "Hobbys",
+        "en": "Hobby",
+        "ru": "Хобби",
+    },
+    "hobby-heater1": {
+        "de": "Hobbys",
+        "en": "Hobby",
+        "ru": "Хобби",
+    },
+    "hobby1": {
+        "de": "Bücher lesen, wissenschaftlichen und historischen Artikeln",
+        "en": "Read books, scientific and historical articles",
+        "ru": "Чтение книг, научных и исторических статей",
+    },
+    "hobby2": {
+        "de": "Ich fotografiere gern",
+        "en": "I like to take pictures",
+        "ru": "Люблю фотографировать.",
+    },
+    "hobby3": {
+        "de": "Ich fotografiere, Architektur-, Natur und Sahen.",
+        "en": "I take to pictures architecture, nature, objects.",
+        "ru": "Фотографирую архитектуру, природу, предметы.",
+    },
+    "hobby4": {
+        "de": "Elektronik, Computer, Audiogeräte, Internetzwerke",
+        "en": "Electronics, computers, audio equipment, internet networks",
+        "ru": "Увлекаюсь электроникой, компьютерами, аудиотехникой.",
+    }, 
+    "hobby5": {
+        "de": "Autodiagnose und -reparatur.",
+        "en": "Car diagnostics and repair.",
+        "ru": "Диагностика и ремонт автомобилей.",
+    },
+    "hobby6": {
+        "de": "Restaurierung verschiedener Sachen",
+        "en": "Restoration of various things",
+        "ru": "Реставрация раличных вещей",
+    },
+    "info-footer": {
+        "de": "Alle Informationen auf dieser Website dienen dazu, jegliche Entwicklung zu verhindern",
+        "en": "All information on this site belongs to its developer",
+        "ru": "Вся информация на этом сайте пренадлежит ее разработчику ",
+    },
+    "hardware-footer": {
+        "de": "Für die Entwicklung der Website wurde ein HP-Laptop mit Intel-Prozessor verwendet.",
+        "en": "An HP laptop with an Intel processor was used to develop the site.",
+        "ru": "Для разрабтки сайта использовался ноутбук фирмы HP на базе процессора intel.",
+    },
+
+    "software-footer": {
+        "de": "Für die Entwicklung dieser Website wurden das Betriebssystem Linux Ubuntu 20.04.6 LTS und die kostenlose Software Visual Studio Code (VSC) verwendet.",
+        "en": "The Linux Ubuntu 20.04.6 LTS operating system and free Visual Studio Code (VSC) software were used to develop this site.",
+        "ru": "Для разрабтки этого сайта была использована операционная система Linux Ubuntu 20.04.6 LTS и бесплатное ПО Visual Studio Code (VSC).",
+    },
+   
+
+   
+
+
+};
